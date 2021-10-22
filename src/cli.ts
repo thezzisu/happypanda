95ac665d1c664dd9602dd2e7188118d2e1cb19be
thezzisu <thezzisu@gmail.com>
2021-10-22 17:03:20 +0800

    v0.0.7

--- src/cli.ts
+++ src/cli.ts
@@ -4,18 +4,21 @@ import { NodeAdapter } from './adapter'
 import { Crawler } from './crawler'
 import CAC from 'cac'
 import fs from 'fs-extra'
+import { join } from 'path'
 
 const cli = CAC()
 
 cli
-  .command('download <url> <dist>', 'Download a E-Hentai gallery')
+  .command('download <url>', 'Download a E-Hentai gallery')
+  .option('-o, --output', 'Output folder')
   .option('-O, --origin', 'Download origin image, not resampled')
-  .action(async (url, dist, config) => {
+  .action(async (url, config) => {
     try {
       const crawler = new Crawler(new NodeAdapter())
       const gallery = await crawler.gallery.getGallery(url)
       console.log(`Downloading ${gallery.title}`)
       console.log(`Gallery tags: ${gallery.tags.join(',')}`)
+      const dist = config.output ?? join(process.cwd(), gallery.title)
       await fs.ensureDir(dist)
       await crawler.downloader.downloadGallery(gallery, dist, config.origin)
     } catch (e) {
